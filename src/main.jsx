6718d4c4dfa1c1485d084f1023580a7bb86c2b08
Cassandra Peterson <cgdesigns93@gmail.com>
2025-08-20 21:47:39 -0500

    configure modals

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './components/App/App'
 import './index.css'
+import './assets/fonts/cabinet-grotesk.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
