efdf5846885f6ddf6a79473880d5161e992011ee
Cassandra Peterson <cgdesigns93@gmail.com>
2025-08-26 10:53:30 -0500

    fix open modal

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -10,7 +10,7 @@ function ModalWithForm({
   onSubmit,
 }) {
   return (
-    <div className={`modal ${isOpen === "add-garment" && "modal_opened"}`}>
+    <div className={`modal ${isOpen && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button onClick={onClose} type="button" className="modal__close">
