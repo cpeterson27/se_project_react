2f901820b858852bbae0c1aa38faa4f5162478a6
CASSANDRA PETERSON <cgdesigns93@gmail.com>
2025-10-04 00:46:06 -0500

    added heart feature

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,10 +1,19 @@
 import "./ItemCard.css";
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { useState } from 'react';
 
 function ItemCard({ item, onCardClick }) {
+const [liked, setLiked] = useState(false);
+
+const handleLikeClick = () => {
+  setLiked(prev => !prev);
+};
+
 console.log('Full item object:', item);
   const handleCardClick = () => {
     onCardClick(item);
   };
+
   return (
     <li className="card">
       <h2 className="card__name">{item.name}</h2>
@@ -14,6 +23,11 @@ console.log('Full item object:', item);
         src={item.link}
         alt={item.name}
       />
+      <button
+      onClick={handleLikeClick}
+      className="heart__button"
+      > {liked ? <AiFillHeart /> : <AiOutlineHeart />}
+</button>
     </li>
   );
 }
